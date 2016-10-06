138400dfc299c31a2d75cff41855505f9072af33
Vincent Castelain <vincent.castelain@dbit.lu>
2016-10-06 14:03:01 +0200

    [WIP] docker/npm starting

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,2 @@
 FROM node:4-onbuild
-
-# Create app directory
-#RUN mkdir -p /usr/src/app
-#WORKDIR /usr/src/app
-
-# replace this with your application's default port
 EXPOSE 8080
--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var server = http.createServer(function (request, response) {
     response.end("Hello World\n");
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
+// Listen on port 8080, IP defaults to 127.0.0.1
 server.listen(8080);
 
 // Put a friendly message on the terminal
-console.log("Server running.");
+console.log("Server running...");
